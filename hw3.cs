59971b54b4abddf39d1664547de223bbb1b5ab53
KREIN-Gutay <centinonicole9@gmail.com>
2025-10-01 22:58:47 +0800

    v3

---
+++ hw3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static Dictionary<char, int> GetFrequency(string c)
+    {
+        var freq = new Dictionary<char, int>();
+
+        foreach (char f in c.ToLower())
+        {
+            if (f == ' ' || f == ',')
+                continue;
+
+            if (freq.ContainsKey(f))
+                freq[f]++;
+            else
+                freq[f] = 1;
+        }
+
+        return freq;
+    }
+    
+    static void Main()
+    {
+        Console.Write("Enter string: ");
+        string c = Console.ReadLine();
+
+        var result = GetFrequency(c);
+
+        foreach (var item in result)
+        {
+            Console.WriteLine(item.Key + "=" + item.Value);
+        }
+
+        Console.ReadKey();
+    }
+}
